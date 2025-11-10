2546d9be3b16a9a34be6823bde7c45e18f2bf9f9
Phan Dai Cuong <phancuongtmtlhp@gmail.com>
2025-11-11 06:24:34 +0700

    change relationship between class table and exam table

--- app/schemas/exams.py
+++ app/schemas/exams.py
@@ -4,21 +4,16 @@ from pydantic import BaseModel
 
 
 class ExamCreate(BaseModel):
-    class_id: int
     title: str
     description: Optional[str] = None
-    exam_date: Optional[datetime] = None
-    max_score: float = 10.00
 
 
 class ExamRead(BaseModel):
     exam_id: int
-    class_id: int
     title: str
     description: Optional[str] = None
-    exam_date: Optional[datetime] = None
-    max_score: float
     created_at: datetime
+    updated_at: datetime
 
     class Config:
         from_attributes = True
@@ -27,8 +22,6 @@ class ExamRead(BaseModel):
 class ExamUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
-    exam_date: Optional[datetime] = None
-    max_score: Optional[float] = None
 
 
 class ResultCreate(BaseModel):
