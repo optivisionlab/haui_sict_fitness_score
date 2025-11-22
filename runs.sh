5bd5c76082bd1bb1830671e0b794921f3afc215d
Chu Huy Hoang <chuhuyhoang56@gmail.com>
2025-11-22 15:41:42 +0700

    add: req

--- runs.sh
+++ runs.sh
@@ -2,5 +2,7 @@ export PYTHONPATH=$(pwd)
 export QDRANT_URL="http://localhost:6333"
 export MONGDB_URI="mongodb://10.100.200.119:27017/"
 export MONGDB_DB="test"
+export DEVICE="cuda"
+
 
 python src/main.py
--- src/config/depend.py
+++ src/config/depend.py
@@ -4,8 +4,8 @@ from torchvision.transforms import  Compose, Resize, ToTensor
 from torchvision.transforms import InterpolationMode
 
 
-resnet_embedding = InceptionResnetV1(pretrained=RESNET_EMBEDDING_PATH).eval()
-mtcnn = MTCNN(**MTCNN_CONFIG)
+resnet_embedding = InceptionResnetV1(pretrained=RESNET_EMBEDDING_PATH).to(DEVICE).eval()
+mtcnn = MTCNN(**MTCNN_CONFIG).to(DEVICE)
 to_tensor_transform = Compose([
     Resize((224, 224), interpolation= InterpolationMode.BICUBIC),  
     ToTensor()
