de1518492519cfc1143154b397bd8054df76b917
Vanh <vanhnekdungso74@gmail.com>
2025-11-25 16:36:19 +0700

    feat: add authentication module

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { clearError } from "@/features/auth/authSlice";
+import { useRouter } from "next/navigation";
+import { LoginForm } from "@/components/form/login-form";
+import { toast } from "sonner";
+import { loginUserThunk } from "@/features/auth/authThunks";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const { loading, error, user, access_token } = useAppSelector(
+    (state) => state.auth
+  );
+
+  const handleSubmit = async (data: { username: string; password: string }) => {
+    dispatch(clearError());
+    const result = await dispatch(loginUserThunk(data));
+
+    if (loginUserThunk.rejected.match(result)) {
+      toast.error(result.payload as string);
+    } else {
+      toast.success("Đăng nhập thành công!");
+    }
+  };
+
+  useEffect(() => {
+    if (access_token && user) {
+      router.replace(`/${user.user_role}`);
+    }
+  }, [access_token, user, router]);
+
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm onSubmit={handleSubmit} />
+            {loading && (
+              <p className="text-sm text-gray-500 mt-2">Đang đăng nhập...</p>
+            )}
+            {error && <p className="text-sm text-red-500 mt-2">{error}</p>}
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Login background"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
