d269da7dfad04805bd19479f7ff5a26996f25fa2
PhanDaiCuong <phancuongtmtlhp@gmail.com>
2026-02-25 11:05:21 +0700

    add router demo

--- backend/app/api/endpoints/__init__.py
+++ backend/app/api/endpoints/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.endpoints import users, classes, exams, redis_events, results
+from app.api.endpoints import users, classes, exams, redis_events, results, demo_api 
 
 api_router = APIRouter()
 
@@ -9,3 +9,4 @@ api_router.include_router(classes.router, prefix="/class", tags=["Classes"])
 api_router.include_router(exams.router, prefix="/exam", tags=["Exams"])
 api_router.include_router(redis_events.router, prefix="/redis", tags=["Redis"])
 api_router.include_router(results.router, prefix="/result", tags=["Results"])
+api_router.include_router(demo_api.router, prefix="/demo",tags=["Demo"])
