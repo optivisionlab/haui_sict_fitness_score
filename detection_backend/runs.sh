cc9c4cbca615fa3967faa4d0dbc092595156f42c
hoangchu <chuhuyhoang56@gmail.com>
2026-04-13 16:26:37 +0000

    change docker compose and commit run sh

--- detection_backend/runs.sh
+++ detection_backend/runs.sh
@@ -2,6 +2,7 @@
 
 PYTHONPATH=${PYTHONPATH:-$(pwd)}
 QDRANT_URL=${QDRANT_URL:-"http://localhost:6333"}
+QDRANT_API_KEY=${QDRANT_API_KEY:-"test"}
 MONGDB_URI=${MONGDB_URI:-"mongodb://10.100.200.119:27017/"}
 MONGDB_DB=${MONGDB_DB:-"test"}
 DEVICE=${DEVICE:-"cuda:0"}
@@ -10,6 +11,7 @@ API_PORT=${API_PORT:-"8000"}
 
 export PYTHONPATH
 export QDRANT_URL
+export QDRANT_API_KEY
 export MONGDB_URI
 export MONGDB_DB
 export DEVICE
